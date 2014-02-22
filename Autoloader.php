69a8de6354419e3917f52a0b4d4efed8b8c70e04
Thomas Heuer <projekte@thomas-heuer.eu>
2014-02-23 00:16:01 +0100

    Autoloader code documentation

--- Autoloader.php
+++ Autoloader.php
@@ -42,134 +42,138 @@
  * );
  * </code>
  * 
+ * To use this autoloader create an instance of it:
+ * <code>
+ * new Autoloader( $configArray );
+ * </code>
+ * 
  * @author Thomas Heuer <projekte@thomas-heuer.eu>
  */
 class Autoloader
 {
-	private $classLoaderConfig;
+    private $classLoaderConfig;
 
-	/**
-	 * @param array $configArray will get merged with default config array
-	 */
-	public function __construct( $configArray = array() )
-	{
-		$this->setClassLoaderConfig( $configArray );
+    /**
+     * @param array $configArray will get merged with default config array
+     */
+    public function __construct( $configArray = array() )
+    {
+        $this->setClassLoaderConfig( $configArray );
 
-		/* register autoload function */
-		spl_autoload_register( array( $this, 'classLoader' ), true, true );
-	}
+        /* register autoload function */
+        spl_autoload_register( array( $this, 'classLoader' ), true, true );
+    }
 
-	/**
-	 * @param array $configArray
-	 */
-	private function setClassLoaderConfig( $configArray )
-	{
-		/* merge array into default configuration */
-		$this->classLoaderConfig = array_merge( array(
-			'baseDir' => '.',
-			'includeDirs' => 'src,lib',
-			'skip' => null,
-			'filePattern' => '%s.php,%s.inc',
-			'useNamespaces' => true,
-			'classNameToDir' => false,
-			'classNameToDirDelimiter' => '_',
-			'resolveStaticClassName' => null,
-			'lowercase' => false,
-		), ( array ) $configArray );
-	}
+    /**
+     * @param array $configArray
+     */
+    private function setClassLoaderConfig( $configArray )
+    {
+        /* merge array into default configuration */
+        $this->classLoaderConfig = array_merge( array(
+            'baseDir' => '.',
+            'includeDirs' => 'src,lib',
+            'skip' => null,
+            'filePattern' => '%s.php,%s.inc',
+            'useNamespaces' => true,
+            'classNameToDir' => false,
+            'classNameToDirDelimiter' => '_',
+            'resolveStaticClassName' => null,
+            'lowercase' => false,
+                ), ( array ) $configArray );
+    }
 
-	/**
-	 * A configurable class autoloader.
-	 *
-	 * @param string $className may use namespaces for folders
-	 * @return boolean
-	 * @throws \RuntimeException
-	 */
-	private function classLoader( $className )
-	{
-		if( is_callable( $this->classLoaderConfig['resolveStaticClassName'] ) )
-		{
-			$staticClassname = $this->classLoaderConfig['resolveStaticClassName']( $className );
-			if( false !== $staticClassname )
-			{
-				require_once $staticClassname;
-				return true;
-			}
-		}
+    /**
+     * A configurable class autoloader.
+     *
+     * @param string $className may use namespaces for folders
+     * @return boolean
+     */
+    private function classLoader( $className )
+    {
+        if( is_callable( $this->classLoaderConfig['resolveStaticClassName'] ) )
+        {
+            $staticClassname = $this->classLoaderConfig['resolveStaticClassName']( $className );
+            if( false !== $staticClassname )
+            {
+                require_once $staticClassname;
+                return true;
+            }
+        }
 
-		$includeDirs = $this->getArray( $this->classLoaderConfig['includeDirs'] );
+        $includeDirs = $this->getArray( $this->classLoaderConfig['includeDirs'] );
 
-		if( $this->classLoaderConfig['useNamespaces'] === false )
-		{
-			/* get rid of namespaces */
-			$classNameAndNamespaces = explode( '\\', $className );
-			$className = array_pop( $classNameAndNamespaces );
-		}
-		else
-		{
-			$this->classLoaderConfig['classNameToDir'] = true;
-			$this->classLoaderConfig['classNameToDirDelimiter'] = '\\';
-		}
+        if( $this->classLoaderConfig['useNamespaces'] === false )
+        {
+            /* get rid of namespaces */
+            $classNameAndNamespaces = explode( '\\', $className );
+            $className = array_pop( $classNameAndNamespaces );
+        }
+        else
+        {
+            $this->classLoaderConfig['classNameToDir'] = true;
+            $this->classLoaderConfig['classNameToDirDelimiter'] = '\\';
+        }
 
-		if( $this->classLoaderConfig['lowercase'] == true )
-		{
-			$className = strtolower( $className );
-		}
+        if( $this->classLoaderConfig['lowercase'] == true )
+        {
+            $className = strtolower( $className );
+        }
 
-		/* skip first part of class name, if specified */
-		if( !is_null( $this->classLoaderConfig['skip'] ) && substr( $className, 0, strlen( $this->classLoaderConfig['skip'] ) ) == $this->classLoaderConfig['skip'] )
-		{
-			$className = ltrim( substr( $className, strlen( $this->classLoaderConfig['skip'] ) ), '\\' );
-		}
+        /* skip first part of class name, if specified */
+        if( !is_null( $this->classLoaderConfig['skip'] ) && substr( $className, 0, strlen( $this->classLoaderConfig['skip'] ) ) == $this->classLoaderConfig['skip'] )
+        {
+            $className = ltrim( substr( $className, strlen( $this->classLoaderConfig['skip'] ) ), '\\' );
+        }
 
-		if( $this->classLoaderConfig['classNameToDir'] == true )
-		{
-			$classDirs = explode( $this->classLoaderConfig['classNameToDirDelimiter'], $className );
+        if( $this->classLoaderConfig['classNameToDir'] == true )
+        {
+            $classDirs = explode( $this->classLoaderConfig['classNameToDirDelimiter'], $className );
 
-			$className = array_pop( $classDirs ); // part after last delimiter as class name
+            $className = array_pop( $classDirs ); // part after last delimiter as class name
 
-			$classDirString = implode( DIRECTORY_SEPARATOR, $classDirs );
-			if( !empty( $classDirString ) )
-			{
-				$includeDirs = array_map( function( $value ) use( $classDirString ) {
-					return $value . DIRECTORY_SEPARATOR . $classDirString . DIRECTORY_SEPARATOR;
-				}, $includeDirs );
-			}
-		}
+            $classDirString = implode( DIRECTORY_SEPARATOR, $classDirs );
+            if( !empty( $classDirString ) )
+            {
+                $includeDirs = array_map( function( $value ) use( $classDirString ) {
+                    return $value . DIRECTORY_SEPARATOR . $classDirString . DIRECTORY_SEPARATOR;
+                }, $includeDirs );
+            }
+        }
 
-		/* iterate over specified directories */
-		foreach( $includeDirs as $directory )
-		{
-			/* iterate over specified file pattern */
-			foreach( $this->getArray( $this->classLoaderConfig['filePattern'] ) as $filePattern )
-			{
-				$realDirectory = realpath( $this->classLoaderConfig['baseDir'] . DIRECTORY_SEPARATOR . $directory ) . DIRECTORY_SEPARATOR;
-				$fileName = $realDirectory . sprintf( $filePattern, $className );
-				if( file_exists( $fileName ) )
-				{
-					require_once $fileName;
-					return true;
-				}
-			}
-		}
-	}
+        /* iterate over specified directories */
+        foreach( $includeDirs as $directory )
+        {
+            /* iterate over specified file pattern */
+            foreach( $this->getArray( $this->classLoaderConfig['filePattern'] ) as $filePattern )
+            {
+                $realDirectory = realpath( $this->classLoaderConfig['baseDir'] . DIRECTORY_SEPARATOR . $directory ) . DIRECTORY_SEPARATOR;
+                $fileName = $realDirectory . sprintf( $filePattern, $className );
+                if( file_exists( $fileName ) )
+                {
+                    require_once $fileName;
+                    return true;
+                }
+            }
+        }
+    }
 
-	/**
-	 * Returns an array. If the value is scalar,
-	 * it will get exploded by $delimiter.
-	 *
-	 * @param mixed $value
-	 * @param string $delimiter (default: ',')
-	 * @return array
-	 */
-	private function getArray( $value, $delimiter = ',' )
-	{
-		// return arrays and objects as arrays
-		if( !is_scalar( $value ) )
-		{
-			return ( array ) $value;
-		}
-		return explode( $delimiter, $value );
-	}
+    /**
+     * Returns an array. If the value is scalar,
+     * it will get exploded by $delimiter.
+     *
+     * @param mixed $value
+     * @param string $delimiter (default: ',')
+     * @return array
+     */
+    private function getArray( $value, $delimiter = ',' )
+    {
+        // return arrays and objects as arrays
+        if( !is_scalar( $value ) )
+        {
+            return ( array ) $value;
+        }
+        return explode( $delimiter, $value );
+    }
 
 }
